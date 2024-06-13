fa3d972a5f91d8959f19e334f6824634a9a468ed
ltown777 <111202761+ltown1@users.noreply.github.com>
2024-06-12 21:22:04 -0400

    Update PrefixManager.java
    Added the actual stuff that lets you change it

--- src/main/java/net/earthmc/leaderprefix/manager/PrefixManager.java
+++ src/main/java/net/earthmc/leaderprefix/manager/PrefixManager.java
@@ -5,6 +5,9 @@ public class PrefixManager {
     public String getValidLeaderPrefixOrNull(String letter) {
         if (letter.equalsIgnoreCase("k") || letter.equals("Ⓚ")) return "Ⓚ";
         if (letter.equalsIgnoreCase("q") || letter.equals("Ⓠ")) return "Ⓠ";
+        if (letter.equalsIgnoreCase("p") || letter.equals("Ⓟ")) return "Ⓟ";
+        if (letter.equalsIgnoreCase("e") || letter.equals("Ⓔ")) return "Ⓔ";
+      
 
         return null;
     }
