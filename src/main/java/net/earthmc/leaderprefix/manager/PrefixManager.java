dfb2267e5c8631733031363a3eb2e1fea116182b
Inj <108689527+Tuteee@users.noreply.github.com>
2024-08-03 18:17:01 -0400

    Update PrefixManager.java

--- src/main/java/net/earthmc/leaderprefix/manager/PrefixManager.java
+++ src/main/java/net/earthmc/leaderprefix/manager/PrefixManager.java
@@ -3,11 +3,10 @@ package net.earthmc.leaderprefix.manager;
 public class PrefixManager {
 
     public String getValidLeaderPrefixOrNull(String letter) {
-        if (letter.equalsIgnoreCase("k") || letter.equals("Ⓚ")) return "🜲";
-        if (letter.equalsIgnoreCase("q") || letter.equals("Ⓠ")) return "🜲";
-        if (letter.equalsIgnoreCase("p") || letter.equals("Ⓟ")) return "Ⓟ";
-        if (letter.equalsIgnoreCase("e") || letter.equals("Ⓔ")) return "🜲";
-
+        if (letter.equalsIgnoreCase("k") || letter.equals("Ⓚ")) return "§6👑";
+        if (letter.equalsIgnoreCase("q") || letter.equals("Ⓠ")) return "§d👑";
+        if (letter.equalsIgnoreCase("p") || letter.equals("Ⓟ")) return "§dⓅ";
+        if (letter.equalsIgnoreCase("e") || letter.equals("Ⓔ")) return "§9👑";
         return null;
     }
 }
