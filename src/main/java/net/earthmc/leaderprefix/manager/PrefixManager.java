9510fa963a13d851d5473783c1da0df8308a974d
Inj <108689527+Tuteee@users.noreply.github.com>
2024-08-03 17:56:59 -0400

    Addition of Crowns instead of letters

--- src/main/java/net/earthmc/leaderprefix/manager/PrefixManager.java
+++ src/main/java/net/earthmc/leaderprefix/manager/PrefixManager.java
@@ -3,11 +3,10 @@ package net.earthmc.leaderprefix.manager;
 public class PrefixManager {
 
     public String getValidLeaderPrefixOrNull(String letter) {
-        if (letter.equalsIgnoreCase("k") || letter.equals("Ⓚ")) return "Ⓚ";
-        if (letter.equalsIgnoreCase("q") || letter.equals("Ⓠ")) return "Ⓠ";
+        if (letter.equalsIgnoreCase("k") || letter.equals("Ⓚ")) return "🜲";
+        if (letter.equalsIgnoreCase("q") || letter.equals("Ⓠ")) return "🜲";
         if (letter.equalsIgnoreCase("p") || letter.equals("Ⓟ")) return "Ⓟ";
-        if (letter.equalsIgnoreCase("e") || letter.equals("Ⓔ")) return "Ⓔ";
-      
+        if (letter.equalsIgnoreCase("e") || letter.equals("Ⓔ")) return "🜲";
 
         return null;
     }
